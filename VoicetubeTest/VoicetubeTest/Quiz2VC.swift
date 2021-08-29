e2129a33c9dbd78c64e80480b31461c6bdfc873b
Hill <angelblue065@gmail.com>
2021-08-29 19:00:16 +0800

    feat : Quiz2 timer and ui

--- VoicetubeTest/VoicetubeTest/Quiz2VC.swift
+++ VoicetubeTest/VoicetubeTest/Quiz2VC.swift
@@ -7,23 +7,78 @@
 
 import UIKit
 
+
 class Quiz2VC: UIViewController {
 
+    private var timer = Timer()
+    
+    @IBOutlet weak var mTimeLabel: UILabel!
+    
+    @IBOutlet weak var mTimeTextField: UITextField!
+    
+    private var setupTime:Int?
+    private var currentTime:Int = 0
+
+    @IBOutlet weak var mStartTimeButton: UIButton!
+    @IBAction func startTimeClick(_ sender: UIButton) {
+        
+        guard let text = mTimeTextField.text else {
+            print("mTimeTextField.text error")
+            return
+        }
+        
+        setupTime = Int(text)
+        
+        guard let setupTime = setupTime else {
+            print("set time plz")
+            return
+        }
+        currentTime = setupTime
+
+        startTimer(second: setupTime)
+    }
+    
+    private func startTimer(second:Int) {
+        
+        view.endEditing(true)
+        mTimeTextField.text = ""
+        mStartTimeButton.isHidden = true
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] timer in
+            
+            guard let self = self else { return }
+            
+            self.currentTime = self.currentTime - 1
+            
+            self.mTimeLabel.text = "\(self.currentTime)"
+            
+            if self.currentTime <= 0 {
+                timer.invalidate()
+                self.mStartTimeButton.isHidden = false
+                self.mTimeLabel.text = "count down over"
+            }
+        })
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        viewWillDisappear(animated)
+        
+        timer.invalidate()
     }
-    */
+
+    
 
 }

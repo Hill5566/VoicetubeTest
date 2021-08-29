0770f333a0d2c6b171ce905b61cd759186885947
Hill <angelblue065@gmail.com>
2021-08-29 15:24:26 +0800

    feat : Quiz1VC Quiz2VC

---
+++ VoicetubeTest/VoicetubeTest/Quiz2VC.swift
@@ -0,0 +1,29 @@
+//
+//  Quiz2VC.swift
+//  VoicetubeTest
+//
+//  Created by Hill Lin on 2021/8/29.
+//
+
+import UIKit
+
+class Quiz2VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
